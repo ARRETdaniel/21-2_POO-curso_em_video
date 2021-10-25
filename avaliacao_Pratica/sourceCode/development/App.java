2079aebd44188e02283cbdccca6d7ab910a6c81c
Daniel Terra Gomes <danielterra24@hotmail.com>
2021-10-25 17:21:16 -0300

    updated

--- avaliacao_Pratica/sourceCode/development/App.java
+++ avaliacao_Pratica/sourceCode/development/App.java
@@ -40,11 +40,11 @@ public class App {
         edgeList.add(new Edge(1, vertexList.get(0), vertexList.get(1)));
         edgeList.add(new Edge(1, vertexList.get(1), vertexList.get(2)));
         edgeList.add(new Edge(1, vertexList.get(2), vertexList.get(3)));
-        // edgeList.add(new Edge(-5, vertexList.get(3), vertexList.get(1))); // negative
-        // case
         edgeList.add(new Edge(10, vertexList.get(0), vertexList.get(3)));
 
         BellmanFord algorithm = new BellmanFord(edgeList, vertexList);
         algorithm.shotestPath(vertexList.get(0), vertexList.get(3));
+
+
     }
 }
